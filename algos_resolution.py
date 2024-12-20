762d0fd2d4790333c4599a23b35cd0437e95d2a4
Pranav Chandra <pranavc5@illinois.edu>
2024-12-19 17:18:41 -0800

    fixing resolution graph

--- algos_resolution.py
+++ algos_resolution.py
@@ -2,35 +2,28 @@ import os
 import json
 import matplotlib.pyplot as plt
 
-folder_path = './json_files'
+directory_path = './output'
+pixel_counts = []
+mses = []
 
-pixel_counts = {}
-mse_values = {}
-
-for filename in os.listdir(folder_path):
+for filename in os.listdir(directory_path):
     if filename.endswith('.json'):
-        with open(os.path.join(folder_path, filename), 'r') as file:
-            data = json.load(file)            
-            resolution = data['resolution']
-            pixel_count = resolution[0] * resolution[1]
-            mse_data = data['mses']
-            average_mse = sum(mse_data.values()) / len(mse_data)
-            if pixel_count not in pixel_counts:
-                pixel_counts[pixel_count] = []
-                mse_values[pixel_count] = []
-            pixel_counts[pixel_count].append(pixel_count)
-            mse_values[pixel_count].append(average_mse)
+        file_path = os.path.join(directory_path, filename)        
+        with open(file_path, 'r') as f:
+            data = json.load(f)        
+        resolution = data['resolution']
+        mse_values = data['mses']        
+        pixel_count = resolution[0] * resolution[1]        
+        mse = mse_values.get('Graph_TSP_MST')
+        pixel_counts.append(pixel_count)
+        mses.append(mse)
 
-avg_mse_by_pixel_count = {pixel_count: sum(mse_values[pixel_count]) / len(mse_values[pixel_count]) for pixel_count in mse_values}
-
-sorted_pixel_counts = sorted(avg_mse_by_pixel_count.keys())
-sorted_avg_mse = [avg_mse_by_pixel_count[count] for count in sorted_pixel_counts]
-
-plt.figure(figsize=(10, 6))
-plt.plot(sorted_pixel_counts, sorted_avg_mse, marker='o', linestyle='-', color='b')
+# Plot the pixel count vs MSE
+plt.scatter(pixel_counts, mses)
 plt.xlabel('Resolution (Pixel Count)')
-plt.ylabel('Average MSE')
-plt.title('Average MSE vs Resolution')
+plt.ylabel('MSE')
+plt.title('Resolution vs MSE')
 plt.grid(True)
+plt.savefig("output/resolutions_mses.jpg", dpi=300, bbox_inches='tight')
 # plt.show()
-plt.savefig("resolutions_mses.jpg", dpi=300, bbox_inches='tight')
+
